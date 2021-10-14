f139f75595c22dcf6b5d561d89e7fe027104413a
Jake Standley <jakestandley95@gmail.com>
2021-10-14 15:48:17 -0500

    Initial commit and first 3 sections of project.

---
+++ db/migrate/20211014203418_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.belongs_to :user
+      t.string :title
+      t.string :image_url
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
